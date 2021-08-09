6076bc2ab502d7a3d956dfdd35d4dc611574e74f
xMari <xmaribr@gmail.com>
2021-08-09 20:52:33 -0300

    Aula 01 - MobX

---
+++ lib/home.dart
@@ -0,0 +1,37 @@
+import 'package:aula/controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+class MyHomePage extends StatelessWidget {
+  final controller = Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mobx"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Observer(
+              builder: (_) {
+                return Text(
+                  '${controller.counter}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
